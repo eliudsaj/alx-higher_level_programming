fe013311cd26d8aba60d7cfdc7b12a8d49fa4f6c
eliudsaj <eliudkaranja5@gmail.com>
2023-08-11 06:19:01 -0700

    modify for pycodestyle

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    all_dir = dir(hidden_4)
-    for i in range(0, len(all_dir)):
-        if not (len(all_dir) > 2 and all_dir[i][0] == '_' and all_dir[i][1] == '_'):
-            print(all_dir[i])
+    names = dir(hidden_4)
+    avoid = "__"
+    for i in range(0, len(names)):
+        if avoid not in names[i]:
+            print(names[i])
