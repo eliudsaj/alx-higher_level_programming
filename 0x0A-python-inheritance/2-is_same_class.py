b11dcf2130cf786c37777f6e3c6cbea147255427
eliudsaj <eliudkaranja5@gmail.com>
2023-09-13 05:11:16 -0700

    class defination

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -2,7 +2,7 @@
 """Defination of a  is_kind_of_class() function."""
 
 
-def is_kind_of_class(obj, a_class):
+def is_same_class(obj, a_class):
     """Checks if an object is an instance of a class.
 
     Args:
