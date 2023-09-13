382aa048256cb5ca0ee8857ad513294434912e91
eliudsaj <eliudkaranja5@gmail.com>
2023-09-13 06:00:57 -0700

    hot fix

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,9 +5,8 @@
 def write_file(filename="", text=""):
     """Calculates the number of lines in a file.
 
-    contains arguments and the filename The name and path of the file to append.
+    contains arguments and the filename The name and path of the file to print.
     Returns the number of lines.
-    return number of characters appended
     """
     with open(filename, "w", encoding="utf-8") as f:
         return f.write(text)
---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Definition of the function."""
+
+
+def write_file(filename="", text=""):
+    """append a utf8 file at the end of the file.
+
+    contains arguments and the filename The name and path of the file to append
+    """
+    with open(filename, "a", encoding="utf-8") as f:
+        return f.write(text)
