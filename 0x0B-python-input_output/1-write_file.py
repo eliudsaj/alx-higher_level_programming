7891ed39fd8628f3c8ab542da9706c2e2055ca53
eliudsaj <eliudkaranja5@gmail.com>
2023-09-13 05:55:28 -0700

    funcion to append file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,8 +5,9 @@
 def write_file(filename="", text=""):
     """Calculates the number of lines in a file.
 
-    contains arguments and the filename The name and path of the file to print.
+    contains arguments and the filename The name and path of the file to append.
     Returns the number of lines.
+    return number of characters appended
     """
     with open(filename, "w", encoding="utf-8") as f:
         return f.write(text)
