6ce10b529ab1fc30d4e40c2c939cb92abd84490f
Eliud Karanja Ndiritu <eliudkaranja5@gmail.com>
2023-10-06 08:59:30 +0300

    Delete 0x0B-python-input_output directory

--- 0x0B-python-input_output/6-load_from_json_file.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""Definition of a JSON file-reading function."""
-import json
-
-
-def load_from_json_file(filename):
-    """Creating a Python object from a JSON file."""
-    with open(filename) as f:
-        return json.load(f)
--- 0x0B-python-input_output/8-class_to_json.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-"""Definition of a Python class-to-JSON function."""
-
-
-def class_to_json(obj):
-    """Returns the dictionary represntation of a data structure."""
-    return obj.__dict__
